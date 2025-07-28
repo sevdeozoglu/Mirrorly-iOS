ad7088f8762d0310d0fb834ee3e24a7bec5daa90
sevdeozoglu <sevdeozoglu@hotmail.com>
2025-07-27 21:43:20 -0400

    Initial Commit

---
+++ Mirrorly/MirrorlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorlyApp.swift
+//  Mirrorly
+//
+//  Created by Sevde Ozoglu on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MirrorlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MirrorlyTests/MirrorlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorlyTests.swift
+//  MirrorlyTests
+//
+//  Created by Sevde Ozoglu on 7/27/25.
+//
+
+import Testing
+@testable import Mirrorly
+
+struct MirrorlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
